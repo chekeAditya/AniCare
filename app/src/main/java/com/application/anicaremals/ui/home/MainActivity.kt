adbe6d142de9b160935e5d7e9e4cda2b3e20ffa4
Aditya Cheke <adityacheke4@gmail.com>
2021-11-19 17:22:34 +0530

    main branch updated

--- app/src/main/java/com/application/anicaremals/ui/home/MainActivity.kt
+++ app/src/main/java/com/application/anicaremals/ui/home/MainActivity.kt
@@ -1,11 +1,13 @@
-package com.application.anicaremals
+package com.application.anicaremals.ui.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.application.anicaremals.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
